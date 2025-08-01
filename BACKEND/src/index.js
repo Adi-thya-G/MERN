f2942f52837b2812939510ea4b114731cf85af83
Adithya karmarkar <adithyakg31@gmail.com>
2025-08-01 23:38:30 +0530

    frontend

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://mern-six-psi.vercel.app/",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   }
 });
