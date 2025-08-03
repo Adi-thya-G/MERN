5a57819e67e2db8ab0ce2bb471c08809895d29e9
Adithya karmarkar <adithyakg31@gmail.com>
2025-08-03 15:25:09 +0530

    navigate

--- BACKEND/src/middleware/multer.middleware.js
+++ BACKEND/src/middleware/multer.middleware.js
@@ -1,9 +1,10 @@
 import multer from 'multer'
-
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(__filename);
 const storage=multer.diskStorage({
   destination:function (req,file,cb)
   {
-    cb(null,"./public/temp")
+    cb(null,path.join(__dirname, 'public', 'temp'))
   },
   filename:function(req,file,cb)
   {
